732df8da6d30c1ce759f031cadc8fa0e1ab6c0ea
Ahoi-Priscilla-Chol <a.majak@alustudent.com>
2026-06-02 17:48:00 +0200

    fix directory validation logic

--- hospital_setup.sh
+++ hospital_setup.sh
@@ -3,13 +3,13 @@ setup_environment() {
 echo "Checking system directories..."
 [ ! -d active_logs ] && mkdir active_logs
 [ ! -d archived_logs ] && mkdir archived_logs
-[ ! -d reports ] $$ mkdir reports
+[ ! -d reports ] && mkdir reports
 }
 secure_environment() {
 chmod 700 active_logs
 ls -ld active_logs
 }
-# Member 3 - Dev0ps Orchestrator
+# Member 3 - DevOp Orchestrator
 setup_environment
 secure_environment
 echo "SYSTEM READY FOR OPERATIONS"
