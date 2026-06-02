69c3aad6a7ce282d5dbfb2472c37ef5520df3f5f
Ahoi-Priscilla-Chol <a.majak@alustudent.com>
2026-06-02 16:59:13 +0200

    fix setup syntax and orchestration logic

---
+++ hospital_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+setup_environment() {
+echo "Checking system directories..."
+[ ! -d active_logs ] && mkdir active_logs
+[ ! -d archived_logs ] && mkdir archived_logs
+[ ! -d reports ] $$ mkdir reports
+}
+secure_environment() {
+chmod 700 active_logs
+ls -ld active_logs
+}
+# Member 3 - Dev0ps Orchestrator
+setup_environment
+secure_environment
+echo "SYSTEM READY FOR OPERATIONS"
+date
